00355561ba27a572a63d1f87e37804a40bde7d7b
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2023-09-13 17:29:25 -0300

    Sections

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const items = document.querySelectorAll('.c-item');
+    const sections = document.querySelectorAll('section');
 
     items.forEach(item => {
         item.addEventListener('click', (e) => {
             e.preventDefault();
             items.forEach(i => i.classList.remove('is-active'));
             item.classList.add('is-active');
+
+            sections.forEach(section => section.classList.add('hide-section'));
+
+            const targetSectionId = item.getAttribute('data-target-section');
+            if (targetSectionId) {
+                const targetSection = document.getElementById(targetSectionId);
+                if (targetSection) {
+                    targetSection.classList.remove('hide-section');
+                }
+            }
         });
     });
 });
